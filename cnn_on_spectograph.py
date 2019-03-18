5d64d4f81948c31fed9e74d6a2ec2beb26992066
Spencer Peterson <spencerjpeterson@gmail.com>
2019-03-17 20:09:46 -0700

    cnn changes

--- cnn_on_spectograph.py
+++ cnn_on_spectograph.py
@@ -1,6 +1,9 @@
 import os
 import zipfile
 import shutil
+import cluster_helper as ch
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 base_dir = '/run/media/spencer/Elements/Documents/CMPS144/img2048nolog'
 genres = ['acoustic', 'classical', 'disco', 'electronic', 'hip-hop', 'indie', 'jazz', 'pop', 'r&b']
@@ -16,120 +19,114 @@ import matplotlib.image as mpimg
 from tensorflow.keras import layers
 from tensorflow.keras import Model
 
-# Input is 1292x1024x3
-img_input = layers.Input(shape=(1292//4, 1024//4, 3))
+def generators():
+    # Data generation
 
-x = layers.Conv2D(16, 3, activation='relu')(img_input)
-x = layers.MaxPooling2D(2)(x)
+    # All images will be rescaled by 1./255
+    datagen = ImageDataGenerator(rescale=1./255, validation_split=0.33)
 
-x = layers.Conv2D(32, 3, activation='relu')(x)
-x = layers.MaxPooling2D(2)(x)
+    # Flow training images in batches of 20 using train_datagen generator
+    train_generator = datagen.flow_from_directory(
+            base_dir,  # This is the source directory for training images
+            target_size = (1292//4, 1024//4),  # All images will be resized to 150x150
+            batch_size = 20,
+            class_mode = 'categorical',
+            subset = 'training')
 
-x = layers.Conv2D(64, 3, activation='relu')(x)
-x = layers.MaxPooling2D(2)(x)
+    validation_generator = datagen.flow_from_directory(
+            base_dir,  # This is the source directory for training images
+            target_size = (1292//4, 1024//4),  # All images will be resized to 150x150
+            batch_size = 20,
+            class_mode = 'categorical',
+            subset = 'validation')
 
-# Flatten feature map to a 1-dim tensor so we can add fully connected layers
-x = layers.Flatten()(x)
+    return train_generator, validation_generator
 
-x = layers.Dense(512, activation='relu')(x)
 
-x = layers.Dropout(0.5)(x)
+def model_and_history(train_generator, validation_generator):
 
-# Create output layer with 9 softmax nodes, one for each category.
-output = layers.Dense(9, activation='sigmoid')(x)
+    # Input is 1292x1024x3
+    img_input = layers.Input(shape=(1292//4, 1024//4, 3))
 
-# Create model:
-# input = input feature map
-# output = input feature map + stacked convolution/maxpooling layers + fully 
-# connected layer + sigmoid output layer
-model = Model(img_input, output)
+    x = layers.Conv2D(16, 3, activation='relu')(img_input)
+    x = layers.MaxPooling2D(2)(x)
 
-"""Let's summarize the model architecture:"""
+    x = layers.Conv2D(32, 3, activation='relu')(x)
+    x = layers.MaxPooling2D(2)(x)
 
-model.summary()
+    x = layers.Conv2D(64, 3, activation='relu')(x)
+    x = layers.MaxPooling2D(2)(x)
 
-"""The "output shape" column shows how the size of your feature map evolves in each successive layer. The convolution layers reduce the size of the feature maps by a bit due to padding, and each pooling layer halves the feature map.
+    # Flatten feature map to a 1-dim tensor so we can add fully connected layers
+    x = layers.Flatten()(x)
 
-Next, we'll configure the specifications for model training. We will train our model with the `binary_crossentropy` loss, because it's a binary classification problem and our final activation is a sigmoid. We will use the `rmsprop` optimizer with a learning rate of `0.001`. During training, we will want to monitor classification accuracy.
+    x = layers.Dense(512, activation='relu')(x)
 
-**NOTE**: In this case, using the [RMSprop optimization algorithm](https://wikipedia.org/wiki/Stochastic_gradient_descent#RMSProp) is preferable to [stochastic gradient descent](https://developers.google.com/machine-learning/glossary/#SGD) (SGD), because RMSprop automates learning-rate tuning for us. (Other optimizers, such as [Adam](https://wikipedia.org/wiki/Stochastic_gradient_descent#Adam) and [Adagrad](https://developers.google.com/machine-learning/glossary/#AdaGrad), also automatically adapt the learning rate during training, and would work equally well here.)
-"""
+    x = layers.Dropout(0.75)(x)
 
-from tensorflow.keras.optimizers import RMSprop
+    # Create output layer with 9 softmax nodes, one for each category.
+    output = layers.Dense(9, activation='softmax')(x)
 
-model.compile(loss = 'categorical_crossentropy',
-              optimizer = RMSprop(lr = 0.001),
-              metrics = ['acc'])
+    # Create model:
+    # input = input feature map
+    # output = input feature map + stacked convolution/maxpooling layers + fully
+    # connected layer + sigmoid output layer
+    model = Model(img_input, output)
 
-"""Let's set up data generators that will read pictures in our source folders, convert them to `float32` tensors, and feed them (with their labels) to our network. We'll have one generator for the training images and one for the validation images. Our generators will yield batches of 20 images of size 150x150 and their labels (binary).
+    """Let's summarize the model architecture:"""
 
-As you may already know, data that goes into neural networks should usually be normalized in some way to make it more amenable to processing by the network. (It is uncommon to feed raw pixels into a convnet.) In our case, we will preprocess our images by normalizing the pixel values to be in the `[0, 1]` range (originally all values are in the `[0, 255]` range).
+    model.summary()
 
-In Keras this can be done via the `keras.preprocessing.image.ImageDataGenerator` class using the `rescale` parameter. This `ImageDataGenerator` class allows you to instantiate generators of augmented image batches (and their labels) via `.flow(data, labels)` or `.flow_from_directory(directory)`. These generators can then be used with the Keras model methods that accept data generators as inputs: `fit_generator`, `evaluate_generator`, and `predict_generator`.
-"""
+    model.compile(loss = 'categorical_crossentropy',
+                  optimizer = RMSprop(lr = 0.0001),
+                  metrics = ['acc'])
 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+    history = model.fit_generator(
+            train_generator,
+            steps_per_epoch = 100,  # 2000 images = batch_size * steps
+            epochs = 10,
+            validation_data = validation_generator,
+            validation_steps = 50,  # 1000 images = batch_size * steps
+            verbose = 2,
+            use_multiprocessing=True)
 
-# All images will be rescaled by 1./255
-datagen = ImageDataGenerator(rescale=1./255, validation_split=0.33)
+    return model, history
 
-# Flow training images in batches of 20 using train_datagen generator
-train_generator = datagen.flow_from_directory(
-        base_dir,  # This is the source directory for training images
-        target_size = (1292//4, 1024//4),  # All images will be resized to 150x150
-        batch_size = 20,
-        class_mode = 'categorical',
-        subset = 'training')
+if __name__ == '__main__':
+    train_set, val_set = generators()
+    model, history = model_and_history(train_set, val_set)
+    # Evaluation
 
-validation_generator = datagen.flow_from_directory(
-        base_dir,  # This is the source directory for training images
-        target_size = (1292//4, 1024//4),  # All images will be resized to 150x150
-        batch_size = 20,
-        class_mode = 'categorical',
-        subset = 'validation')
+    # Retrieve a list of accuracy results on training and test data
+    # sets for each training epoch
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    print(acc)
+    print(val_acc)
 
-"""##### Training
-Let's train on all 2,000 images available, for 15 epochs, and validate on all 1,000 test images. (This may take a few minutes to run.)
-"""
+    # Retrieve a list of list results on training and test data
+    # sets for each training epoch
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
 
-history = model.fit_generator(
-      train_generator,
-      steps_per_epoch = 100,  # 2000 images = batch_size * steps
-      epochs = 15,
-      validation_data = validation_generator,
-      validation_steps = 50,  # 1000 images = batch_size * steps
-      verbose = 2)
+    # Get number of epochs
+    epochs = range(len(acc))
 
-"""### Evaluating Accuracy and Loss for the Model
+    # Plot training and validation accuracy per epoch
+    plt.plot(epochs, acc)
+    plt.plot(epochs, val_acc)
+    plt.title('Training and validation accuracy')
 
-Our simple CNN achieved 70% accuracy on the validation set. Let's plot the training/validation accuracy and loss as collected during training:
-"""
+    plt.figure()
 
-# Retrieve a list of accuracy results on training and test data
-# sets for each training epoch
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-print(acc)
-print(val_acc)
+    # Plot training and validation loss per epoch
+    plt.plot(epochs, loss)
+    plt.plot(epochs, val_loss)
+    plt.title('Training and validation loss')
 
-# Retrieve a list of list results on training and test data
-# sets for each training epoch
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+    # Eval labels
+    #pred_label = model.predict_generator(validation_generator,
+    #        workers=8, use_multiprocessing=True)
 
-# Get number of epochs
-epochs = range(len(acc))
-
-# Plot training and validation accuracy per epoch
-plt.plot(epochs, acc)
-plt.plot(epochs, val_acc)
-plt.title('Training and validation accuracy')
-
-plt.figure()
-
-# Plot training and validation loss per epoch
-plt.plot(epochs, loss)
-plt.plot(epochs, val_loss)
-plt.title('Training and validation loss')
-
-exit(0)
+    #print('######### Labels #########')
+    #print(pred_label)
